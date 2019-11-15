0e0f281a1c4f7323f929b0977870ff891215611e
Mega Soegiarto <soegiarto.mega@gmail.com>
2019-11-15 10:33:57 -0800

    change landing page image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,23 +5,48 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import image2 from '../images/marie-bellando-mitjans-cBChXxQqEKM-unsplash.jpg';
+import image from '../images/document-translation45.jpg';
+
 
 const useStyles = makeStyles(theme => ({
   card: {
     maxWidth: '100vw',
-    maxHeight: '92vh',
     boxShadow: 'none',
-    opacity: '0.8',
+  },
+  action: {
+    maxHeight: '82vh',
   },
   media: {
-    height: '82vh',
+    height: '112vh',
+    [theme.breakpoints.down('md')]: {
+      opacity: '0.3',
+    },
   },
-  myText: {
+  title: {
     position: 'absolute',
-    top: '20vh',
+    top: '50vh',
+    left: '5vw',
+    textShadow:
+      '-1px -1px 0 #fff, 1px -1px 0 #fff,-1px 1px 0 #fff,1px 1px 0 #fff, 1px 1px 4px #673ab7',
+    [theme.breakpoints.down('sm')]: {
+      left: 0,
+      top: '20vh',
+    }
+  },
+  description: {
+    position: 'absolute',
+    fontSize: '20px',
+    top: '40vh',
+    padding: '0 2.5rem',
+    textShadow:
+      '-1px -1px 0 #fff, 1px -1px 0 #fff,-1px 1px 0 #fff,1px 1px 0 #fff, 2px 2px 0 #ccc',
+    letterSpacing: '1px',
     [theme.breakpoints.up('md')]: {
+      top: '62vh',
       left: '5vw',
+      padding: '0',
+      textShadow:
+        '-1px -1px 1px #fff, 1px -1px 1px #fff,-1px 1px 1px #fff,1px 1px 1px #fff'
     },
   }
 }));
@@ -31,18 +56,19 @@ const Home = () => {
 
   return (
     <Card className={classes.card}>
-      <CardActionArea>
+      <CardActionArea className={classes.action}>
         <CardMedia
           className={classes.media}
-          image={image2}
+          image={image}
         />
-        <Typography component="h1" variant="h3" className={classes.myText}>Let's get this sheet done</Typography>
-        <CardContent>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Photo by <a href="https://unsplash.com/@gabianspirit?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Marie Bellando-Mitjans</a> on <a href="https://unsplash.com/photos/cBChXxQqEKM">Unsplash</a>
-          </Typography>
-        </CardContent>
+        <Typography component="h1" variant="h3" className={classes.title}>Let's get this sheet done</Typography>
+        <Typography component="p" className={classes.description}>Translating documents is as easy as uploading and downloading</Typography>
       </CardActionArea>
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Photo by <a href="https://www.translatemedia.com/">translatemedia</a>
+        </Typography>
+      </CardContent>
     </Card>
   );
 }
